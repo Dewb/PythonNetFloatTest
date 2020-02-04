9813c41640d075df989967e803b6eee610c384df
Michael Dewberry <michael.dewberry@autodesk.com>
2020-02-04 17:50:59 -0500

    Initial commit of test case based on Python.Included

---
+++ MyModule/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyModule
+{
+    public class MyClass
+    {
+        public static float Add(float a, float b)
+        {
+            return a + b;
+        }
+        
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ PythonNetFloatTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using Python.Runtime;
+
+namespace PythonNetFloatTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string preamble =
+                "import clr\n" +
+                "clr.AddReference('MyModule')\n" +
+                "from MyModule import MyClass\n" +
+                "import sys\n" +
+                "print(sys.version)";
+
+            string callWithFloatArgs = "result = MyClass.Add(1.0, 2.0)";
+            string callWithIntArgs = "result = MyClass.Add(1, 2)";
+            string callWithMixedArgs = "result = MyClass.Add(1.0, 2)";
+
+            var installer = new Python.Included.Installer();
+            installer.SetupPython().Wait();
+
+            PythonEngine.Initialize();
+            using (Py.GIL())
+            {
+                using (PyScope scope = Py.CreateScope())
+                {
+                    scope.Exec(preamble);
+                    
+                    scope.Exec(callWithFloatArgs);
+                    System.Console.WriteLine(scope.Get("result")); // prints "3.0"
+
+                    scope.Exec(callWithIntArgs);
+                    System.Console.WriteLine(scope.Get("result")); // prints "3"
+
+                    scope.Exec(callWithMixedArgs); // Python.Runtime.PythonException: 'TypeError : No method matches given arguments for Add'
+                    System.Console.WriteLine(scope.Get("result"));
+                }
+            }
+        }
+    }
+}
